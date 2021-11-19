495d860efe5e433a44588301ee19ba86799743e2
jackiestchen <jackiestchen@gmail.com>
2021-11-18 21:39:36 -0700

    Change timer into a fucntion call

--- timer1.js
+++ timer1.js
@@ -1,10 +1,13 @@
 const arg = process.argv.splice(2);
 
-for (const el of arg) {
-  if (el > 0 && Number(el)) {
-    setTimeout(() => {
+const timer = (time) => {
+  for (const el of time) {
+    if (el > 0 && Number(el)) {
+      setTimeout(() => {
       process.stdout.write('\x07');
-    }, el*1000);
+      }, el * 1000);
+    }
   }
-}
+};
 
+timer(arg);
